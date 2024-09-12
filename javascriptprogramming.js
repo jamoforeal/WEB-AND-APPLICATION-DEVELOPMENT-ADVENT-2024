566895211978308cf4e765c2e784eaff2971b303
jamoforeal <raymondjames031404@gmail.com>
2024-09-12 18:09:20 +0300

    Add files via upload

---
+++ javascriptprogramming.js
@@ -0,0 +1,38 @@
+// This is my first js application
+console.log("Helllo world")
+
+// variables
+
+// initialization
+var my_name = "Raymond james";
+
+// Declaration of a variable
+var height;
+
+// Assign a variable
+height = 3.4;
+console.log(my_name);
+console.log(height)
+console.log(my_name);
+
+// Declaration of a variable
+var height = "4'3";
+
+//Defining a variable
+var my_name = "Raymond James";
+console.log("Hello world")
+var height = "4'3"
+let
+
+
+
+
+
+
+
+
+
+
+
+
+
