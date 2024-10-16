497804ac591b8c1610146da856c7bb37c06c1506
jamoforeal <raymondjames031404@gmail.com>
2024-10-16 09:33:40 +0300

    Add files via upload

---
+++ Function that adds,subtract,multiply and divides/Week 5/week5.js
@@ -0,0 +1,35 @@
+//Function that adds,subtracts,multiplies and divides values
+
+let a =10;
+// Assign the value 5 to b
+let b = 5;
+// Assign the value 2 to c
+let c = 2;
+// Assign the value a + b to c:
+
+
+//Adding
+let x = 5;
+let y = 2;
+let z = x + y;
+console.log(x,y)
+
+//Subtraction
+let u = 5;
+let v = 2;
+let w = u - v;
+console.log(u,v)
+
+//Multiplication
+let o = 5;
+let p = 2;
+let q = o * p;
+console.log(o,p)
+
+//Division
+let e = 5;
+let f = 2;
+let g = e%f;
+console.log(e,f)
+
+
