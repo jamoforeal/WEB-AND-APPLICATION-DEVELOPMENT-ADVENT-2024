5981bd8c76d4751eca4b7bde99085b5365aea055
jamoforeal <raymondjames031404@gmail.com>
2024-10-16 09:27:54 +0300

    Add files via upload

---
+++ .github/Revision.js
@@ -0,0 +1,15 @@
+ // a function that texts my name and greets you in your mother tongue.
+ function greet(){
+    var name = "James";
+    var Luganda = "Olyotya James";
+    var Runyankole = "Olirota james";
+    var Swahili = "Mambovipii James";
+    var French = "Commen Sava James"
+    if (name = "James"){
+        console.log(Luganda);
+        console.log(Runyankole);
+        console.log(Swahili);
+        console.log(French);
+    }
+   }
+greet();
